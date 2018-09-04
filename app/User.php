82f57ff3dd4b25af04d82f6b1ed7f503bf47691d
hongxu liu <hlsw1994@qq.com>
2018-09-04 23:57:38 +0800

    引入jwt

--- app/User.php
+++ app/User.php
@@ -2,28 +2,31 @@
 
 namespace App;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * Get the identifier that will be stored in the subject claim of the JWT.
      *
-     * @var array
+     * @return mixed
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
 
     /**
-     * The attributes that should be hidden for arrays.
+     * Return a key value array, containing any custom claims to be added to the JWT.
      *
-     * @var array
+     * @return array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
